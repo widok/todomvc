a5b522740f5266c4eb064484902cb50ee091c550
Tim Nieradzik <tim@kognit.io>
2014-10-07 01:28:31 +0200

    Delete routing

--- src/main/scala/org/widok/todomvc/pages/NotFound.scala
+++
@@ -1,9 +0,0 @@
-package org.widok.todomvc.pages
-
-import org.widok._
-import org.widok.bindings.HTML._
-
-case class NotFound() extends Page {
-  def contents() = Heading.Level1("Not found!")
-  def ready(route: InstantiatedRoute) { }
-}
