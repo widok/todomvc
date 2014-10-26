f2318deba6df52b9a5e6d936f8a15d7a0e3b5975
Tim Nieradzik <tim@kognit.io>
2014-10-26 19:48:25 +0100

    Bump to widok 0.1.0

--- project/Build.scala
+++ project/Build.scala
@@ -4,18 +4,24 @@ import scala.scalajs.sbtplugin.ScalaJSPlugin._
 
 object Build extends sbt.Build {
   val buildOrganisation = "org.widok"
-  val buildVersion = "0.1-SNAPSHOT"
+  val buildVersion = "0.1.0"
   val buildScalaVersion = "2.11.2"
-  val buildScalaOptions = Seq("-unchecked", "-deprecation", "-encoding", "utf8")
+  val buildScalaOptions = Seq(
+    "-unchecked", "-deprecation",
+    "-encoding", "utf8"
+  )
 
   lazy val main = Project(id = "todomvc", base = file("."))
     .settings(scalaJSSettings: _*)
     .settings(
+      resolvers += "Sonatype Nexus Staging" at "https://oss.sonatype.org/service/local/staging/deploy/maven2",
       libraryDependencies ++= Seq(
-        "org.widok" %%% "widok" % "0.1-SNAPSHOT"),
+        "org.widok" %%% "widok" % "0.1.0"
+      ),
       organization := buildOrganisation,
       version := buildVersion,
       scalaVersion := buildScalaVersion,
       scalacOptions := buildScalaOptions,
-      ScalaJSKeys.persistLauncher := true)
+      ScalaJSKeys.persistLauncher := true
+    )
 }
