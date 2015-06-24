04d7f8804f699f9237e6b912c2f8792539f89d1a
Tim Nieradzik <tim@kognit.io>
2015-06-24 11:45:48 +0200

    Migrate to Widok v0.2.2

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ import org.scalajs.core.tools.sem._
 
 object Build extends sbt.Build {
   val buildOrganisation = "org.widok"
-  val buildVersion = "0.2.0"
+  val buildVersion = "0.2.2"
   val buildScalaVersion = "2.11.6"
   val buildScalaOptions = Seq(
     "-unchecked"
@@ -18,7 +18,7 @@ object Build extends sbt.Build {
     .enablePlugins(ScalaJSPlugin)
     .settings(
       libraryDependencies ++= Seq(
-        "io.github.widok" %%% "widok" % "0.2.0"
+        "io.github.widok" %%% "widok" % "0.2.2"
       )
     , organization := buildOrganisation
     , version := buildVersion
